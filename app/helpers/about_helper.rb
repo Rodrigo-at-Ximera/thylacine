0b83a5ccbd3968bea82a811126e6662c296f5974
Rodrigo <rodrigo@ximera.com.ar>
2019-12-23 09:52:41 -0300

    Upgrade to Rails 6 - About

--- app/helpers/about_helper.rb
+++ app/helpers/about_helper.rb
@@ -2,14 +2,14 @@
 
 module AboutHelper
   ABOUT_LINKS = {
-    rails:            ['Ruby on Rails (v5.2)', 'http://rubyonrails.org/'],
+    rails:            ['Ruby on Rails (v6.0)', 'http://rubyonrails.org/'],
     postgresql:       ['PostgreSQL (v10.1)', 'https://www.postgresql.org/'],
-    devise:           ['Devise (v4.4)', 'https://github.com/plataformatec/devise'],
-    omniauth:         ['OmniAuth (v1.8)', 'https://github.com/omniauth/omniauth'],
-    cancancan:        ['CanCanCan (v2.1)', 'https://github.com/CanCanCommunity/cancancan'],
+    devise:           ['Devise (v4.7)', 'https://github.com/plataformatec/devise'],
+    omniauth:         ['OmniAuth (v1.9)', 'https://github.com/omniauth/omniauth'],
+    cancancan:        ['CanCanCan (v3.0)', 'https://github.com/CanCanCommunity/cancancan'],
     exifr:            ['EXIF Reader (v1.3)', 'https://github.com/remvee/exifr'],
-    rest_client:      ['REST Client (v2.0)', 'https://github.com/rest-client/rest-client'],
-    bootstrap:        ['Bootstrap (v4.0)', 'https://getbootstrap.com/'],
+    rest_client:      ['REST Client (v2.1)', 'https://github.com/rest-client/rest-client'],
+    bootstrap:        ['Bootstrap (v4.4)', 'https://getbootstrap.com/'],
     jquery:           ['jQuery (v1.12)', 'https://jquery.com/'],
     jqueryui:         ['jQuery UI (v1.12)', 'http://jqueryui.com/'],
     google_maps:      ['Google Maps API (v3)', 'https://developers.google.com/maps/'],
@@ -18,7 +18,7 @@ module AboutHelper
     heroku:           ['Heroku', 'https://www.heroku.com/'],
     linkedin:         ['LinkedIn', 'https://www.linkedin.com/in/rodrigocharon/'],
     upwork:           ['Upwork', 'https://www.upwork.com/o/profiles/users/_~01b1339734965265fb/'],
-    rspec:            ['RSpec (v3.7)', 'http://rspec.info/']
+    rspec:            ['RSpec (v3.9)', 'http://rspec.info/']
   }.freeze
 
   def about_link(key)
